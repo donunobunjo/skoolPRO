5e7cee57ab68ef917f320b8f083df005d69c82cf
donunobunjo <don.unobunjo@gmail.com>
2019-04-09 11:27:54 +0100

    Implemented the score migration

--- database/migrations/2019_04_09_095738_create_scores_table.php
+++ database/migrations/2019_04_09_095738_create_scores_table.php
@@ -15,6 +15,22 @@ class CreateScoresTable extends Migration
     {
         Schema::create('scores', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('RollNumber',15);
+            $table->string('FullName',30);
+            $table->string('Session',15);
+            $table->string('Term',15);
+            $table->string('Class',15);
+            //$table->string('Category',20);
+            $table->string('Subject',20);
+            $table->tinyInteger('FirstCA')->unsigned()->default(0);
+            $table->tinyInteger('SecondCA')->unsigned()->default(0);
+            $table->tinyInteger('Exam')->unsigned()->default(0);
+            $table->Integer('Total')->unsigned()->default(0);
+            $table->Integer('GrandTotal')->unsigned()->default(0);
+            $table->Integer('Average')->unsigned()->default(0);
+            $table->Integer('GrandAverage')->unsigned()->default(0);
+            $table->Integer('Position')->unsigned()->default(0);
+            $table->Integer('GrandPosition')->unsigned()->default(0);
             $table->timestamps();
         });
     }
